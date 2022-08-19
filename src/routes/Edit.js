37c4e7b7fb469c427300fd3ff0ca20a83b2a166e
Timemash <timemash24@gmail.com>
2022-08-20 01:50:36 +0900

    Update: edit name and tag of checked bookmark

--- src/routes/Edit.js
+++ src/routes/Edit.js
@@ -1,26 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
 import { connect } from 'react-redux/es/exports';
 import { addBookmarks } from '../routes/store';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import * as IndxdDBController from '../components/IndxdDBController';
 
 function Edit({ bookmarks }) {
-  const [checkedItems, setCheckedItems] = useState([]);
+  const navigate = useNavigate();
+  const [list, setList] = useState([]);
+  const [checkedIds, setCheckedIds] = useState([]);
   const [itemName, setItemName] = useState('');
-  const [itemTag, setItemTag] = useState('');
+  const [itemTags, setItemTags] = useState([]);
 
-  const checkHandler = ({ target }) => {
+  useEffect(() => {
+    console.log(bookmarks);
+    setList(bookmarks);
+  }, [list]);
+
+  const handleCheckbox = ({ target }) => {
     const id = parseInt(target.id);
 
-    if (checkedItems.includes(id)) {
-      if (checkedItems.length > 1)
-        setItemName(bookmarks.find((bm) => bm.id === checkedItems[0]).name);
+    if (checkedIds.includes(id)) {
+      if (checkedIds.length > 1)
+        setItemName(bookmarks.find((bm) => bm.id === checkedIds[0]).name);
       else setItemName('');
-      setCheckedItems(checkedItems.filter((itemId) => itemId !== id));
+      setCheckedIds(checkedIds.filter((itemId) => itemId !== id));
     } else {
       console.log('add id');
-      setCheckedItems([...checkedItems, id]);
+      setCheckedIds([...checkedIds, id]);
       setItemName(bookmarks.find((bm) => bm.id === id).name);
+      setItemTags(bookmarks.find((bm) => bm.id === id).tags);
     }
   };
 
@@ -28,12 +40,37 @@ function Edit({ bookmarks }) {
     setItemName(target.value);
   };
 
-  const tagInputHandler = ({ target }) => {
-    setItemTag(target.value);
+  const tagInputHandler = (e, index) => {
+    console.log(e.target.value);
+
+    const tags = [...itemTags];
+    const newTags = tags.map((tag, i) => {
+      if (i === index) return e.target.value;
+      else return tag;
+    });
+    setItemTags(newTags);
   };
 
-  console.log(checkedItems);
-  console.log(bookmarks);
+  const removeBookmarkBtn = (e, id) => {
+    e.preventDefault();
+    // setNewBookmarks(newBookmarks.filter((bookmark) => bookmark.id !== id));
+  };
+
+  const updateBookmark = (id) => {
+    IndxdDBController.updateDBValue('name', id, itemName);
+    IndxdDBController.updateDBValue('tags', id, itemTags);
+  };
+
+  const handleSavebtn = (e) => {
+    e.preventDefault();
+
+    // 개별 변경
+    if (checkedIds.length === 1) {
+      updateBookmark(checkedIds[0]);
+    }
+    navigate('/');
+  };
+
   return (
     <div>
       <Navbar />
@@ -41,45 +78,48 @@ function Edit({ bookmarks }) {
         <section>
           <p>
             <label htmlFor="name">이름</label>
-            {checkedItems.length <= 1 ? (
+            {checkedIds.length <= 1 ? (
               <input
                 type="text"
                 name="name"
                 value={itemName}
                 onChange={nameInputHandler}
-                disabled={checkedItems.length ? false : true}
+                disabled={checkedIds.length ? false : true}
               />
             ) : null}
           </p>
           <div>
             <label htmlFor="tags">태그</label>
             {bookmarks.map((bm, i) =>
-              checkedItems.length === 1 && bm.id === checkedItems[0] ? (
+              checkedIds.length === 1 && bm.id === checkedIds[0] ? (
                 <p key={`checked${i}`}>
-                  {bm.tags.map((tag, j) => (
+                  {bm.tags.map((_, j) => (
                     <input
                       key={`checkedTag${j}`}
                       name="tag"
-                      value={tag}
-                      onChange={tagInputHandler}
+                      value={itemTags[j]}
+                      onChange={(e) => tagInputHandler(e, j)}
                     />
                   ))}
                 </p>
               ) : null
             )}
-            {checkedItems.length > 1 ? <input type="text" name="tags" /> : null}
-            <button>수정하기</button>
+            {checkedIds.length > 1 ? <input type="text" name="tags" /> : null}
+            <button onClick={handleSavebtn}>변경사항 저장✨</button>
           </div>
         </section>
-        <section>
+        <ul>
           {bookmarks.map((bm, i) => (
-            <div key={`checkbox${i}`}>
-              <input id={bm.id} type="checkbox" onChange={checkHandler} />
+            <li key={`checkbox${i}`}>
+              <input id={bm.id} type="checkbox" onChange={handleCheckbox} />
               <span>이름 : {bm.name}</span>
               <span>태그 : {bm.tags.map((tag) => `# ${tag} `)}</span>
-            </div>
+              <button onClick={(e) => removeBookmarkBtn(e, bm.id)}>
+                <FontAwesomeIcon icon={faTrashCan} />
+              </button>
+            </li>
           ))}
-        </section>
+        </ul>
       </form>
     </div>
   );
