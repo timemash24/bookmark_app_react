cdbaabf044888b571999a149bb3087862a43e8bc
Timemash <timemash24@gmail.com>
2022-08-20 13:04:15 +0900

    Update: Add same tag to checked bookmarks

--- src/routes/Edit.js
+++ src/routes/Edit.js
@@ -14,6 +14,7 @@ function Edit({ bookmarks }) {
   const [checkedIds, setCheckedIds] = useState([]);
   const [itemName, setItemName] = useState('');
   const [itemTags, setItemTags] = useState([]);
+  const [tagToAdd, setTagToAdd] = useState('');
   const [validTag, setValidTag] = useState(true);
 
   useEffect(() => {
@@ -37,11 +38,11 @@ function Edit({ bookmarks }) {
     }
   };
 
-  const nameInputHandler = ({ target }) => {
+  const handleNameInput = ({ target }) => {
     setItemName(target.value);
   };
 
-  const tagInputHandler = (e, index) => {
+  const handleTagInput = (e, index) => {
     console.log(e.target.value);
 
     const tags = [...itemTags];
@@ -61,17 +62,47 @@ function Edit({ bookmarks }) {
     // setNewBookmarks(newBookmarks.filter((bookmark) => bookmark.id !== id));
   };
 
+  const handleNewTag = (e) => {
+    setTagToAdd(e.target.value);
+  };
+
   const updateBookmark = (id) => {
     IndxdDBController.updateDBValue('name', id, itemName);
     IndxdDBController.updateDBValue('tags', id, itemTags);
   };
 
+  const addTags = (ids, newTag) => {
+    for (const id of ids) {
+      let newTags = [];
+      for (const bm of list) {
+        if (bm.id === id) {
+          const tags = bm.tags;
+          newTags = [...tags, newTag];
+          IndxdDBController.updateDBValue('tags', id, newTags);
+          break;
+        }
+      }
+    }
+  };
+
   const handleSavebtn = (e) => {
     e.preventDefault();
+    if (!validTag) return;
 
     // 개별 변경
-    if (checkedIds.length === 1 && validTag) {
+    if (checkedIds.length === 1) {
       updateBookmark(checkedIds[0]);
+      navigate('/');
+    }
+
+    // 태그 일괄 추가
+    else {
+      console.log(tagToAdd);
+      addTags(checkedIds, tagToAdd);
+      // checkedIds.forEach((id) => {
+
+      //   IndxdDBController.updateDBValue('tags', id, itemTags);
+      // })
       navigate('/');
     }
   };
@@ -81,43 +112,57 @@ function Edit({ bookmarks }) {
       <Navbar />
       <form>
         <section>
-          <p>
-            <label htmlFor="name">이름</label>
-            {checkedIds.length <= 1 ? (
+          {checkedIds.length === 1 ? (
+            <div>
+              <label htmlFor="name">이름</label>
               <input
                 type="text"
                 name="name"
                 value={itemName}
-                onChange={nameInputHandler}
+                onChange={handleNameInput}
                 disabled={checkedIds.length ? false : true}
               />
-            ) : null}
-          </p>
+            </div>
+          ) : null}
           <div>
-            <label htmlFor="tags">태그</label>
             {bookmarks.map((bm, i) =>
               checkedIds.length === 1 && bm.id === checkedIds[0] ? (
-                <p key={`checked${i}`}>
+                <div key={`checked${i}`}>
                   {bm.tags.map((_, j) => (
-                    <input
-                      key={`checkedTag${j}`}
-                      name="tag"
-                      value={itemTags[j]}
-                      onChange={(e) => tagInputHandler(e, j)}
-                    />
+                    <p>
+                      <label htmlFor="tag">태그</label>
+                      <input
+                        key={`checkedTag${j}`}
+                        name="tag"
+                        value={itemTags[j]}
+                        onChange={(e) => handleTagInput(e, j)}
+                      />
+                    </p>
                   ))}
-                </p>
+                </div>
               ) : null
             )}
             {validTag ? null : <p>⚠ need at least one tag ⚠</p>}
-            {checkedIds.length > 1 ? <input type="text" name="tags" /> : null}
-            <button onClick={handleSavebtn}>변경사항 저장✨</button>
+            {checkedIds.length > 1 ? (
+              <p>
+                <label htmlFor="newTag">태그</label>
+                <input type="text" name="newTag" onChange={handleNewTag} />
+              </p>
+            ) : null}
+            {checkedIds.length > 0 ? (
+              <button onClick={handleSavebtn}>변경사항 저장✨</button>
+            ) : null}
           </div>
         </section>
         <ul>
           {bookmarks.map((bm, i) => (
             <li key={`checkbox${i}`}>
-              <input id={bm.id} type="checkbox" onChange={handleCheckbox} />
+              <input
+                id={bm.id}
+                type="checkbox"
+                value={tagToAdd}
+                onChange={handleCheckbox}
+              />
               <span>이름 : {bm.name}</span>
               <span>태그 : {bm.tags.map((tag) => `# ${tag} `)}</span>
               <button onClick={(e) => removeBookmarkBtn(e, bm.id)}>
